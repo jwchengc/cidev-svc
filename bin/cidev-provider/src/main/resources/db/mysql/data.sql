62336cdb0b97a8c90f3cc9d26631a5621088bfb8
jwzhangd <jwzhangd@isoftstone.com>
2016-10-10 17:53:29 +0800

    remove files

--- bin/cidev-provider/src/main/resources/db/mysql/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `demo` (`id`,`name`,`created_at`,`updated_at`) VALUES ('40287f815741815d01574181a4020000','JiaZaibo','2016-09-19 16:13:18','2016-09-19 16:13:18');
--- bin/cidev-provider/src/main/resources/db/mysql/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `demo` (
-  `id` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
