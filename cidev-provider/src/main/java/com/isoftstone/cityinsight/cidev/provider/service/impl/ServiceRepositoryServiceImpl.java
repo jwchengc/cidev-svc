886c708409a2e7c0f3938fe0c84dc5511b2976cf
jwzhangd <jwzhangd@isoftstone.com>
2016-10-11 17:59:38 +0800

    add file

---
+++ cidev-provider/src/main/java/com/isoftstone/cityinsight/cidev/provider/service/impl/ServiceRepositoryServiceImpl.java
@@ -0,0 +1,28 @@
+package com.isoftstone.cityinsight.cidev.provider.service.impl;
+
+import java.util.List;
+
+import com.isoftstone.cityinsight.cidev.api.domain.Application;
+import com.isoftstone.cityinsight.cidev.api.service.IServiceRepositoryService;
+
+public class ServiceRepositoryServiceImpl implements IServiceRepositoryService {
+
+	@Override
+	public List<Application> getRecommendServices(String userId, Integer pageNum, Integer pageSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Application> getAllServices(Integer pageNum, Integer pageSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Application getServiceDetail(String appId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
