b91c9962c7edaa0ba89ec7a5a33f45c8975b5f47
jwzhangd <jwzhangd@isoftstone.com>
2016-10-11 20:33:43 +0800

    update files

--- cidev-provider/src/main/java/com/isoftstone/cityinsight/cidev/provider/dao/mapper/ApplicationMapper.java
+++ cidev-provider/src/main/java/com/isoftstone/cityinsight/cidev/provider/dao/mapper/ApplicationMapper.java
@@ -1,5 +1,12 @@
 package com.isoftstone.cityinsight.cidev.provider.dao.mapper;
 
+import java.util.List;
+import java.util.Map;
+
+import com.isoftstone.cityinsight.cidev.api.domain.Application;
+
 public interface ApplicationMapper {
+	
+	public List<Application> selectRecommendServices(Map<String, Object> params);
 
 }
