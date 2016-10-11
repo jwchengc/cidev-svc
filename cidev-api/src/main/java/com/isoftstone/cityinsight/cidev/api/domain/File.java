6fb4ebc170e63627475d0d92bf24143fb78d918e
jwzhangd <jwzhangd@isoftstone.com>
2016-10-11 14:30:06 +0800

    add file

---
+++ cidev-api/src/main/java/com/isoftstone/cityinsight/cidev/api/domain/File.java
@@ -0,0 +1,72 @@
+package com.isoftstone.cityinsight.cidev.api.domain;
+
+import java.util.Date;
+
+public class File {
+	
+	private String fileId;
+	private String fileName;
+	private String filePath;
+	private String fileSize;
+	private String fileSuffix;
+	private String fileContentType;
+	private String fileUrl;
+	private String createdBy;
+	private Date createdAt;
+	
+	public String getFileId() {
+		return fileId;
+	}
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public String getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(String fileSize) {
+		this.fileSize = fileSize;
+	}
+	public String getFileSuffix() {
+		return fileSuffix;
+	}
+	public void setFileSuffix(String fileSuffix) {
+		this.fileSuffix = fileSuffix;
+	}
+	public String getFileContentType() {
+		return fileContentType;
+	}
+	public void setFileContentType(String fileContentType) {
+		this.fileContentType = fileContentType;
+	}
+	public String getFileUrl() {
+		return fileUrl;
+	}
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
