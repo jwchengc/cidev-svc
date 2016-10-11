9d0862b20d92d7e0fbdea4a121e07194205f47dc
jwzhangd <jwzhangd@isoftstone.com>
2016-10-11 14:13:45 +0800

    add file

---
+++ cidev-api/src/main/java/com/isoftstone/cityinsight/cidev/api/domain/ApplicationVersion.java
@@ -0,0 +1,92 @@
+package com.isoftstone.cityinsight.cidev.api.domain;
+
+import java.util.Date;
+
+public class ApplicationVersion {
+	private String versionId;
+	private String appId;
+	private String appVersion;
+	private String description;
+	private String versionInfo;
+	private String keyword;
+	private String copyright;
+	private String linkMan;
+	private String mobile;
+	private String address;
+	private String createdBy;
+	private Date createdAt;
+	
+	public String getVersionId() {
+		return versionId;
+	}
+	public void setVersionId(String versionId) {
+		this.versionId = versionId;
+	}
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	public String getAppVersion() {
+		return appVersion;
+	}
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getVersionInfo() {
+		return versionInfo;
+	}
+	public void setVersionInfo(String versionInfo) {
+		this.versionInfo = versionInfo;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	public String getCopyright() {
+		return copyright;
+	}
+	public void setCopyright(String copyright) {
+		this.copyright = copyright;
+	}
+	public String getLinkMan() {
+		return linkMan;
+	}
+	public void setLinkMan(String linkMan) {
+		this.linkMan = linkMan;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
