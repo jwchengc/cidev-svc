d0d4943820b7391b9041d3076830183cb9c1047d
jwzhangd <jwzhangd@isoftstone.com>
2016-10-11 17:56:35 +0800

    add methods

--- cidev-api/src/main/java/com/isoftstone/cityinsight/cidev/api/service/IComponentRepositoryService.java
+++ cidev-api/src/main/java/com/isoftstone/cityinsight/cidev/api/service/IComponentRepositoryService.java
@@ -1,5 +1,14 @@
 package com.isoftstone.cityinsight.cidev.api.service;
 
-public interface IComponentRepositoryService {
+import java.util.List;
 
+import com.isoftstone.cityinsight.cidev.api.domain.Application;
+
+public interface IComponentRepositoryService {
+	
+	public List<Application> getRecommendComponents(String userId, Integer pageNum, Integer pageSize);
+	
+	public List<Application> getAllComponents(Integer pageNum, Integer pageSize);
+	
+	public Application getComponentsDetail(String appId);
 }
