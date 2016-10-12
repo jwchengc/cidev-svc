906a6bd75c673f15a3835640480d2f41c120ab63
jwzhangd <jwzhangd@isoftstone.com>
2016-10-12 14:14:28 +0800

    append component repository func

---
+++ cidev-provider/src/main/java/com/isoftstone/cityinsight/cidev/provider/service/impl/ComponentRepositoryServiceImpl.java
@@ -0,0 +1,29 @@
+package com.isoftstone.cityinsight.cidev.provider.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.isoftstone.cityinsight.cidev.api.domain.Application;
+import com.isoftstone.cityinsight.cidev.api.service.IComponentRepositoryService;
+
+public class ComponentRepositoryServiceImpl implements IComponentRepositoryService {
+
+	@Override
+	public List<Application> getRecommendComponents(String userId, Integer pageNum, Integer pageSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Application> getAllComponents(Integer pageNum, Integer pageSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getComponentsDetail(String appId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
