7ce41796ba72f170c033f99f8bc8129ad83f8861
jwzhang201501 <jwzhang201501@163.com>
2016-10-31 15:27:42 +0800

    append published channel

--- cidev-api/src/main/java/com/isoftstone/cityinsight/cidev/api/domain/Application.java
+++ cidev-api/src/main/java/com/isoftstone/cityinsight/cidev/api/domain/Application.java
@@ -1,7 +1,6 @@
 package com.isoftstone.cityinsight.cidev.api.domain;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * Created by jeff on 10/11/16.
@@ -15,13 +14,22 @@ public class Application extends ApplicationVersion implements Serializable{
     private String appName;
     private String appType;
     private String svcCategoryId;
+    private Integer publishedChannel;
 //    private Date createdAt;
 //    private String createdBy;
     
     //非持久化属性
     private String fileUrl;
 
-    public String getFileUrl() {
+    public Integer getPublishedChannel() {
+		return publishedChannel;
+	}
+
+	public void setPublishedChannel(Integer publishedChannel) {
+		this.publishedChannel = publishedChannel;
+	}
+
+	public String getFileUrl() {
 		return fileUrl;
 	}
 
